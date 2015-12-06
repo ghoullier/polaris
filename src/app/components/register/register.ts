ed9504d441b7bc08d2ef6ac924433d3b98011867
ghoullier <gregory.houllier@gmail.com>
2015-12-07 00:00:20 +0100

    feat(register) Update component

--- src/app/components/register/register.ts
+++ src/app/components/register/register.ts
@@ -1,4 +1,4 @@
-import { Component } from 'angular2/core';
+import { Component, ViewEncapsulation} from 'angular2/core';
 
 
 @Component({
@@ -7,7 +7,8 @@ import { Component } from 'angular2/core';
   styleUrls: ['app/components/register/register.css'],
   providers: [],
   directives: [],
-  pipes: []
+  pipes: [],
+  encapsulation: ViewEncapsulation.None
 })
 export class Register {
 
