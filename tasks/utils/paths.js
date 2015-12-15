8079742013ae26bc166fd4b2b5f6ca32f8eb35df
ghoullier <gregory.houllier@gmail.com>
2015-12-15 09:13:32 +0100

    chore(config) Use templated strings to config folders

--- tasks/utils/paths.js
+++ tasks/utils/paths.js
@@ -1,25 +1,29 @@
+const BASE_CONFIG_PATH = './config/'
+const BASE_SOURCE_PATH = './src/'
+const BASE_DIST_PATH = './dist/'
+
 export default {
   sources: {
-    root: './src/',
-    bower: './bower.json',
-    config: './config/config-{{env}}.json',
+    root: `${BASE_SOURCE_PATH}`,
+    bower: `./bower.json`,
+    config: `${BASE_CONFIG_PATH}config-{{env}}.json`,
     entries: {
-      app: './src/app.js',
-      config: './src/config.js',
-      vendor: './src/vendor.js',
-      html: './src/index.html'
+      app: `${BASE_SOURCE_PATH}app.js`,
+      config: `${BASE_SOURCE_PATH}config.js`,
+      vendor: `${BASE_SOURCE_PATH}vendor.js`,
+      html: `${BASE_SOURCE_PATH}index.html`
     },
-    scripts: './src/{components,shared}/**/*.js',
-    styles: './src/{components,shared}/**/styles/main.scss',
-    images: './assets/images/**/*.png',
-    fonts: './assets/fonts/**',
-    partials: './src/{components,shared}/**/*.html'
+    scripts: `${BASE_SOURCE_PATH}{components,shared}/**/*.js`,
+    styles: `${BASE_SOURCE_PATH}{components,shared}/**/styles/main.scss`,
+    images: `${BASE_SOURCE_PATH}assets/images/**/*.png`,
+    fonts: `${BASE_SOURCE_PATH}assets/fonts/**`,
+    partials: `${BASE_SOURCE_PATH}{components,shared}/**/*.html`
   },
   dist: {
-    root: './dist/',
-    scripts: './dist/js/',
-    styles: './dist/css/',
-    images: './dist/css/images/',
-    fonts: './dist/fonts/'
+    root: `${BASE_DIST_PATH}`,
+    scripts: `${BASE_DIST_PATH}js/`,
+    styles: `${BASE_DIST_PATH}css/`,
+    images: `${BASE_DIST_PATH}css/images/`,
+    fonts: `${BASE_DIST_PATH}fonts/`
   }
 };
